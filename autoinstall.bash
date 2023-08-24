57f6aea219d2edbbc02834ee8787fd5d7ced2ba5
womanlifefreedom13 <134485033+womanlifefreedom13@users.noreply.github.com>
2023-08-24 10:35:09 +0300

    Create autoinstall.bash

---
+++ autoinstall.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# بررسی اگر کاربر دسترسی root نداشته باشد، اسکریپت خاتمه می‌یابد.
+if [ "$(id -u)" -ne 0 ]; then
+    echo 'This script must be run as root' >&2
+    exit 1
+fi
+
+# بروزرسانی سیستم
+apt update
+apt upgrade -y
+
+# نصب nginx
+apt install -y nginx
+
+# نصب curl اگر نصب نبود
+apt install -y curl
+
+# اجرای دستور نصب مورد نظر
+bash <(curl -Ls https://raw.githubusercontent.com/therealaleph/xrayr-reality/main/install.sh)
+
+# پیامی برای نصب کامل نمایش داده می‌شود.
+echo "Installation completed successfully."
